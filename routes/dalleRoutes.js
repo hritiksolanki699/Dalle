fe85426ac1cdf7c91dfba67d7e0a8f83c326392f
Hritik Solanki <hritiksolanki699@gmail.com>
2024-02-16 19:17:10 +0530

    Update dalleRoutes.js

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -20,12 +20,11 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.images.generate({
+    const aiResponse = await openai.createImage({
       model="dall-e-3",
       prompt,
-      size="1024x1024",
-      quality="standard",
-      n=1,
+      n: 1,
+      size: "1024x1024",
       response_format: 'b64_json',
     });
 
